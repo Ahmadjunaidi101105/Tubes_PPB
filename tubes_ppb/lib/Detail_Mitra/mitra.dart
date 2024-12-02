17db2fcac9be39b99f0dd2a14a10cf47ee19fbe9
alamfauzan <gfauzan963@gmail.com>
2024-12-02 22:22:51 +0700

    test

---
+++ tubes_ppb/lib/Detail_Mitra/mitra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailmitraPage extends StatefulWidget {
+  const DetailmitraPage({super.key});
+
+  @override
+  State<DetailmitraPage> createState() => _DetailmitraPageState();
+}
+
+class _DetailmitraPageState extends State<DetailmitraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
